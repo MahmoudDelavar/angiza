39e30289cee5354a1c66a24da0a2175017e64d2d
mahmoud.D.P <mahmouddp@gmail.com>
2023-08-22 20:30:21 +0330

    add font and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className='App'>angize negar challenge</div>;
+  return (
+    <>
+      <div className='text-center mt-2'>angize negar challenge</div>
+
+      <p>اسیدستنی</p>
+    </>
+  );
 }
 
 export default App;
