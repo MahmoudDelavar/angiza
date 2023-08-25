30427ad45d6123787373a251282b1e354646cf43
mahmoud.D.P <mahmouddp@gmail.com>
2023-08-25 21:26:55 +0330

    change some styles

--- src/component/products/propduct-card.js
+++ src/component/products/propduct-card.js
@@ -3,8 +3,8 @@ import './style.css';
 const ProductCard = ({ name, brief, description, image }) => {
   return (
     <>
-      <div className=' col-xxl-2 col-lg-3 col-md-4 m-2 shadow'>
-        <div className='card-box'>
+      <div className=' col-xxl-2 col-lg-3 col-md-4 col-sm-6 m-2 shadow'>
+        <div className='card-box mx-auto '>
           <div className='img-contain'>
             <img className='product-img' src={image} alt='product-img' />
           </div>
