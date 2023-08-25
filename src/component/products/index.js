9fc11022a815ed876240c8d607b06f1ce35a3298
mahmoud.D.P <mahmouddp@gmail.com>
2023-08-25 19:51:55 +0330

    change product list according to adding redux

--- src/component/products/index.js
+++ src/component/products/index.js
@@ -1,31 +1,38 @@
 import { useEffect, useState } from 'react';
-import { getAllProducts } from '../../lib/requests';
+// import { getAllProducts } from '../../lib/requests';
 import ProductCard from './propduct-card';
-//=========================================
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllproducts } from '../../state-management/actions/product-actions';
+//========================================================
 const ProductList = () => {
-  const [propducts, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const { data, isLoading } = useSelector((state) => state.productState);
 
   //--------- load all propducts---------
   useEffect(() => {
-    (async () => {
-      try {
-        const propducts = await getAllProducts();
-        setProducts(propducts);
-      } catch (error) {
-        console.log('AxiosERR "cant loaded propducts:"', error);
-      }
-    })();
+    try {
+      getAllproducts(dispatch);
+    } catch (error) {
+      console.log('AxiosERR "cant loaded propducts:"', error);
+    }
   }, []);
-
-  if (propducts.length === 0) {
-    return <p className='text-center'>هیچ محصولی یافت نشد</p>;
-  }
 
   return (
     <>
       <div className='container-floid bg-dark pt-3'>
         <div className='row justify-content-center '>
-          {propducts.map((p) => (
+          {/* ----------------loading section--------------- */}
+          {isLoading && (
+            <p className='text-center'> در حال بارگزاری محصولات ...</p>
+          )}
+
+          {/* ----------notfound products section----------- */}
+          {data?.length === 0 && (
+            <p className='text-center badge'>هیچ محصولی یافت نشد</p>
+          )}
+
+          {/*------------- products list section ----------- */}
+          {data?.map((p) => (
             <ProductCard
               key={p.id}
               name={p.name || 'نام محصول'}
